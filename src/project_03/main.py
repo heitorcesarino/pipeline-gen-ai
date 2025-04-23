44782bb4c90801359b167cd0a47c67e11114d932
hcesarino <heitor.cesarino@gmail.com>
2025-04-23 10:42:40 -0300

    simple code block to teste the environment

---
+++ src/project_03/main.py
@@ -0,0 +1,9 @@
+# Simple math calculation to test your setup
+def main():
+    a = 10
+    b = 5
+    result = a + b
+    print(f"The result of {a} + {b} is {result}")
+
+if __name__ == "__main__":
+    main()
